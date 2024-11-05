b0dca6c05629d3ddf94c2ea735f4a7552de3cff7
leonce moussongui <leonce-vicente.moussongui@laplateforme.io>
2024-11-05 13:06:15 +0100

    ajout job 3

---
+++ Jour 3.py
@@ -0,0 +1,54 @@
+'''JOB 2
+a=15
+if a<18:
+    print("vous ne pouvez pas voter.")
+else:
+   print("Vous pouvez voter.")'''
+
+
+#JOB 3
+#for i in range(1,101):
+ #  if i==26 or i==37 or i==88:
+  #     continue
+   # else:
+    #    print(i)
+    
+
+#for i in range(1,101):   
+ #   if i%3==0 and i%5==0:  
+  #      print("FizzBuzz")
+   # elif i%3==0:
+    #    print("Fizz")
+    #elif i%5==0:
+     #   print("Buzz")
+    #else:
+     #   print(i)
+
+
+#JOB 5
+'''for n in range(2, 20):
+    for x in range(2, n):
+       if n % x == 0:
+           continue
+       else:
+        print(n)'''
+
+'''i=2
+is_prime= True
+while i<10:
+    for j in range (2,i):
+        if i%j==0:
+           print(f"{i} est un nbre premier")
+           is_prime=False
+    if is_prime==True:
+        print(f"{i}est premier")'''
+
+'''JOB 7
+a="abcdefghijklmnopqrstuvwxyz"
+i=1
+while i <= len(a):
+    print(a[0:i])
+    i+=1'''
+
+
+
